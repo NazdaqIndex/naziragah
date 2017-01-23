9df5f11888a62f84eedd31acb62174e69538cd5d
Avand Amiri <avand@avandamiri.com>
2017-01-23 00:55:00 -0800

    Whale resets state; card spacing fixed

--- js/application.js
+++ js/application.js
@@ -1,11 +1,20 @@
+function reset() {
+  $(".card").removeClass("card-active");
+  $(".content").removeClass("content-active");
+}
+
 $(function() {
+  $(".yunus-whale").click(function(event) {
+    event.preventDefault();
+    reset();
+  });
+
   $("a.card").click(function(event) {
     event.preventDefault();
 
     var card = $(this);
 
-    $(".card").removeClass("card-active");
-    $(".content").removeClass("content-active");
+    reset();
 
     card.addClass("card-active");
     $(card.attr("href")).addClass("content-active");
